0aa315e4b834d9bbdb14294aac46ba055b323fda
Dhevan Lau <1433596@students.wits.ac.za>
2018-07-26 22:32:58 +0200

    Basic character horizontal movement done

---
+++ Test1/Assets/Scripts/Dhevan/CatController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class CatController : MonoBehaviour {
+
+	private InputDevice joystick;
+
+	public float moveSpeed = 10f; 
+	private float xInput, yInput;
+
+	public SpriteRenderer SR;
+
+	// Use this for initialization
+	void Start () {
+		joystick = InputManager.ActiveDevice;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		checkDirection ();
+		checkMovement ();
+	}
+
+	void checkDirection(){
+		xInput = joystick.LeftStick.X;  
+		yInput = joystick.LeftStick.Y;
+
+		if (Mathf.Abs(xInput) != 0f)
+		{
+			SpriteRenderer SR = GetComponent<SpriteRenderer>();
+			if (xInput < 0f) {
+				SR.flipX = true;
+			}
+			else {
+				SR.flipX = false;
+			}
+		}
+	}
+
+	void checkMovement(){
+		Vector3 Movement = new Vector3(xInput, 0f, 0f) * Time.deltaTime * moveSpeed;
+		this.transform.Translate(Movement, Space.World); 
+	}
+
+
+}
