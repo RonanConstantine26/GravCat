0fe079620edb916cdb91f425976458ae1e986c0c
Ronan Constantine <constantineronan@gmail.com>
2018-07-30 22:42:15 +0200

    Added Jason art assets
    Added jason assets and put into scene

--- Test1/Assets/Scripts/Dhevan/CatController.cs
+++ Test1/Assets/Scripts/Dhevan/CatController.cs
@@ -6,34 +6,50 @@ using InControl;
 public class CatController : MonoBehaviour {
 
 	private InputDevice joystick;
+	private RayCastController RCC;
+	private Character charScript;
+	private Pounce pnce;
 
 	public float moveSpeed = 10f; 
 	private float xInput, yInput;
 
-	public float jumpStrength = 4f;
-	public float fallStrengthFactor = 4f;
-	private bool jumped = false;
-	public float jumpHeight = 1.5f;
-	private Vector3 jumpPos;
-
 	public SpriteRenderer SR;
-	private Rigidbody2D playerRB;
 
 	// Use this for initialization
 	void Start () {
 		joystick = InputManager.ActiveDevice;
-		jumped = false;
-		playerRB = this.GetComponent<Rigidbody2D> ();
+		RCC = gameObject.GetComponent<RayCastController> ();
+		charScript = gameObject.GetComponent<Character> ();
+		pnce = gameObject.GetComponent<Pounce> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		checkJumping ();
-		checkFalling ();
 		checkDirection ();
-		checkMovement ();
-		playerRB.rotation = 0f; //prevent player object from rotating
+
+		if(charScript.canMove && pnce.CheckLevelRot())
+		{
+			if(!RCC.attachRight && !RCC.attachLeft && !RCC.attachRightAny&& !RCC.attachLeftAny)
+			{
+				checkMovement (false,false);
+				print ("1 in");
+			}
+			if(RCC.attachRight || RCC.attachRightAny)
+			{
+				checkMovement (false, true);
+			}
+			if(RCC.attachLeft || RCC.attachLeftAny)
+			{
+				checkMovement (true, false);
+			}
+		}
+	
+
+
+
 	}
+
+
 
 	void checkDirection(){
 		xInput = joystick.LeftStick.X;  
@@ -51,28 +67,58 @@ public class CatController : MonoBehaviour {
 		}
 	}
 
-	void checkMovement(){
-		Vector3 Movement = new Vector3(xInput, 0f, 0f) * Time.deltaTime * moveSpeed;
-		this.transform.Translate(Movement, Space.World); 
-	}
+	void checkMovement(bool isright,bool isleft){
 
-	void checkJumping(){
-		if ((joystick.Action1 || Input.GetKey(KeyCode.Space)) && jumped == false) {
-			playerRB.AddForce(new Vector2(0f, jumpStrength*100f), ForceMode2D.Force);
-			jumped = true;
-			jumpPos = this.transform.position;
-		} 
-	}
-	void checkFalling(){
-
-		if (this.transform.position.y - jumpPos.y >= jumpHeight) {
-			playerRB.AddForce (new Vector2 (0f, -1f * (jumpStrength*50f/fallStrengthFactor)), ForceMode2D.Force);
+		//charScript.RCC.pnce.rb.velocity = Vector2.zero;
+		if(xInput==0)
+		{
+			charScript.isRunningRight = false;
+			charScript.isRunningLeft = false;
 		}
-	}
 
-	void OnCollisionEnter2D(Collision2D other){
-		if (other.gameObject.tag == "Platform") {
-			jumped = false;
+		if(!isright && !isleft)
+		{
+			Vector3 Movement = new Vector3(xInput, 0f, 0f) * Time.deltaTime * moveSpeed;
+			this.transform.Translate(Movement, Space.World); 
+			print ("in 3");
+			if(xInput>0)
+			{
+				charScript.isRunningRight = true;
+				charScript.isRunningLeft = false;
+			}
+			if(xInput<0)
+			{
+				charScript.isRunningRight = false;
+				charScript.isRunningLeft = true;
+			}
+		}
+		else if(isright)
+		{
+			if(xInput>=0)
+			{
+				Vector3 Movement = new Vector3(xInput, 0f, 0f) * Time.deltaTime * moveSpeed;
+				this.transform.Translate(Movement, Space.World); 
+				if(xInput>0)
+				{
+					charScript.isRunningRight = true;
+					charScript.isRunningLeft = false;
+				}
+			}
+
+		}
+		else if(isleft)
+		{
+			if(xInput<=0)
+			{
+				Vector3 Movement = new Vector3(xInput, 0f, 0f) * Time.deltaTime * moveSpeed;
+				this.transform.Translate(Movement, Space.World); 
+				if(xInput<0)
+				{
+					charScript.isRunningRight = false;
+					charScript.isRunningLeft = true;
+				}
+			}
+
 		}
 	}
 
