a11be991185782be8ab1925fa332ba89ef358f15
Dhevan Lau <1433596@students.wits.ac.za>
2018-07-31 21:38:46 +0200

    Main Menu Scene, Scene Controller and Music Controller Added
    The Scene controller has been coded, just have to correctly label the scene names to the ones in the script.  The music controller wont be deleted when changign scenes so any music playing will continue throughout each scene, just have to found a song and put it into the audio source attached to the music controller which is found in the main menu scene.

---
+++ Test1/Assets/Scripts/Dhevan/SceneController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+	public string nextScene;
+
+
+
+	public void OnClickMainMenuScene(){
+		SceneManager.LoadScene ("MainMenuScene");
+	}
+
+	public void OnClickDemoScene1(){
+		SceneManager.LoadScene ("DemoScene1");
+	}
+	public void OnClickDemoScene2(){
+		SceneManager.LoadScene ("DemoScene2");
+	}
+	public void OnClickDemoScene3(){
+		SceneManager.LoadScene ("DemoScene3");
+	}
+	public void OnClickLoadNextScene(){
+		SceneManager.LoadScene (nextScene);
+	}
+	public void OnClickQuitApplication(){
+		Application.Quit ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Scene thisScene = SceneManager.GetActiveScene ();
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (thisScene.name != "MainMenuScene") {
+				SceneManager.LoadScene ("MainMenuScene");
+			} else {
+				Application.Quit ();
+			}
+		}
+	}
+}
